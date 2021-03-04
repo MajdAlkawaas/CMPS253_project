2ca7b102f27d7ec8dda1839fc8d6e9fb32c4e3a1
Majd Alkawaas <mma250@mail.aub.edu>
2021-03-05 00:33:15 +0200

    Created the guest app, built the guest models, and made migrations

---
+++ my_django_app/guest/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+import datetime
+
+
+class Guest(models.Model):
+    PhoneNumber = models.CharField(max_length=50)
+    password   = models.CharField(max_length=50)
+    WalkedAway = models.BooleanField()
+    Kickedout  = models.BooleanField()
+    Served     = models.BooleanField()
+    CreatedAt  = models.DateTimeField(auto_now=True)
+    GuestNumber = models.PositiveSmallIntegerField()
+    beginOfServiceTime = models.DateTimeField(blank=True)
+    endOfServiceTime = models.DateTimeField(blank=True)
+    Customer   = models.ForeignKey('customer.Customer', on_delete=models.CASCADE)
+    Director   = models.ForeignKey('customer.Director', on_delete=models.CASCADE)
+    Queue      = models.ForeignKey('customer.Queue', on_delete=models.CASCADE)
+    Category = models.ForeignKey('customer.Category', on_delete=models.CASCADE)
+    Queueoperator = models.ForeignKey('customer.Queueoperator', on_delete=models.CASCADE)
