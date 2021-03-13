1d98a4a23dd7a6aec1e0c5a9c71a294ec2338cd9
Majd Alkawaas <mma250@mail.aub.edu>
2021-03-14 01:35:53 +0200

    fixed a spelling mistake in the signup url

--- my_django_app/customer/urls.py
+++ my_django_app/customer/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('signin/', views.signin, name="singin-customer-page"),
-    path('signup/', views.signup, name="singup-customer-page"),
+    path('signup/', views.signup, name="signup-customer-page"),
     path('forgot/', views.forgot, name="forgot-customer-page"),
     path('queueSetup/', views.queueSetup, name="queueSetup-customer-page"),
     path('queueManagement/', views.queueManagement, name="queueManagement-customer-page"),
