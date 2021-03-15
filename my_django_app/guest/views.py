485359f0d488875841389834207ac20eafad797c
Majd Alkawaas <mma250@mail.aub.edu>
2021-03-15 16:29:21 +0200

    Adding the urls and views code related to the queue guest page
    Added two urls:
    1) URL for accessing queues using the queue_id
    2) URL for accessing queues using the queue_uuid
    
    Added two views functions:
    1) guest_view_id for accessing queues using the queue_id
    2) guest_view_uuid for accessing queues using the queue_uuid

--- my_django_app/guest/views.py
+++ my_django_app/guest/views.py
@@ -1,4 +1,64 @@
 from django.shortcuts import render
+from customer.models import Customer,Director, Category, Queue
 
-def guest(request):
-    return render(request, 'guest/guest.html') 
+
+
+def guest_view_id(request, queue_id):
+    try:
+        # queue = Customer.objects.get(queue_uuid= queue_uuid)
+        queue = Queue.objects.get(pk= queue_id)
+    except Queue.DoesNotExist:
+        raise Http404("Queue with uuid {} doesn't exit".format(queue_id)) 
+
+    director   = Director.objects.get(pk= queue.Director.id)
+    customer   = Customer.objects.get(pk=director.Customer.id)
+    categories = Category.objects.all().filter(Queue_id=queue_id)
+
+    print(categories)
+    for category in categories:
+        print(category.Name)
+
+    context = {
+        'queue'     : queue,
+        'director'  : director,
+        'customer'  : customer,
+        'categories': categories
+    }
+
+    return render(request, 'guest/guest.html', context) 
+
+
+
+
+
+
+
+
+def guest_view_uuid(request, queue_uuid):
+    queue_uuid_hex = queue_uuid.hex
+    try:
+        queue = Queue.objects.get(queue_uuid= queue_uuid_hex)
+    except Queue.DoesNotExist:
+        raise Http404("cusotmer with uuid {} doesn't exit".format(queue_uuid_hex)) 
+
+    director   = Director.objects.get(pk= queue.Director.id)
+    customer   = Customer.objects.get(pk=director.Customer.id)
+    categories = Category.objects.all().filter(Queue_id=queue.id)
+
+
+    print(categories)
+    for category in categories:
+        print(category.Name)
+
+    context = {
+        'queue'     : queue,
+        'director'  : director,
+        'customer'  : customer,
+        'categories': categories
+    }
+
+    return render(request, 'guest/guest.html', context) 
+
+# def guest(request):
+
+#     return render(request, 'guest/guest.html') 
