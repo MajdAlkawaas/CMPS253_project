dbdec12f0bade8bf5ff2f28208f808b58c6c25e8
Majd Alkawaas <mma250@mail.aub.edu>
2021-03-15 16:14:06 +0200

    Registered the Queue and Category models into the admin page

--- my_django_app/customer/admin.py
+++ my_django_app/customer/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer, Director, Queue
+from .models import Customer, Director, Queue, Category
 admin.site.register(Customer)
 admin.site.register(Director)
 admin.site.register(Queue)
+admin.site.register(Category)
