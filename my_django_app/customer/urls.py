6d90b2e8396b42a7b2ac35b95f29707c1b79abfc
AhmedSadaqa <64624614+AhmedSadaqa@users.noreply.github.com>
2021-03-15 23:25:24 +0200

    Update urls.py
    Updated the urls

--- my_django_app/customer/urls.py
+++ my_django_app/customer/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('signin/', views.signin, name="singin-customer-page"),
-    path('signup/', views.signup, name="singup-customer-page"),
+    path('signin/', views.signin, name="signin-customer-page"),
+    path('signup/', views.signup, name="signup-customer-page"),
     path('forgot/', views.forgot, name="forgot-customer-page"),
     path('queueSetup/', views.queueSetup, name="queueSetup-customer-page"),
     path('queueManagement/', views.queueManagement, name="queueManagement-customer-page"),
     path('edit/', views.edit, name="edit-customer-page"),
+    path('', views.home, name="customer-home"),
 ]
