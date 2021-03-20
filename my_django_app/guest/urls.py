4f898abd224ff0fd50572e62fb22026abc35664d
hre06 <65748557+hre06@users.noreply.github.com>
2021-03-20 21:11:23 +0200

    added the path to the guest urls

--- my_django_app/guest/urls.py
+++ my_django_app/guest/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('queue/uuid/<uuid:queue_uuid>', views.guest_view_uuid, name="queue-page"),
     
     # Request a queue guest page using queue_id
-    path('queue/id/<int:queue_id>', views.guest_view_id, name="queue-page")
+    path('queue/id/<int:queue_id>', views.guest_view_id, name="queue-page"),
+
+    path('guest/', views.guest, name="guest-page")
 ]
