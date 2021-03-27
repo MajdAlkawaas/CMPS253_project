c4475bb0351ec487ac3720f1d44d7a875a04b0cb
Majd Alkawaas <mma250@mail.aub.edu>
2021-03-28 01:07:44 +0300

    the auth testing pages

--- my_django_app/customer/urls.py
+++ my_django_app/customer/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
+
+    path('test/signin/', views.test_signin, name="test-signin-customer-page"),
+    path('test/signup/', views.test_signup, name="test-signup-customer-page"),
+     path('test/welcome/', views.test_welcome, name="test-welcome-customer-page"),
+
     path('signin/', views.signin, name="signin-customer-page"),
+    
     path('signup/', views.signup, name="signup-customer-page"),
     path('forgot/', views.forgot, name="forgot-customer-page"),
     path('queueSetup/', views.queueSetup, name="queueSetup-customer-page"),
