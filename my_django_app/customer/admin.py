91304888f824fb07630caa61c198035e17a1c8c5
Majd Alkawaas <mma250@mail.aub.edu>
2021-03-28 22:33:31 +0300

    Registered the User model to the admin page.

--- my_django_app/customer/admin.py
+++ my_django_app/customer/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer, Director, Queue, Category
+from .models import User, Customer, Director, Queue, Category
+admin.site.register(User)
 admin.site.register(Customer)
 admin.site.register(Director)
 admin.site.register(Queue)
