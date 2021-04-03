b527d06a382653582fd2c1a000e8b5d7640b6c24
Ahmed Sadaqa <afs18@mail.aub.edu>
2021-04-04 01:21:53 +0300

    Register a Queue Operator to the admin page

--- my_django_app/customer/admin.py
+++ my_django_app/customer/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Customer, Director, Queue, Category
+from .models import User, Customer, Director, Queue, Category, Queueoperator
 admin.site.register(User)
 admin.site.register(Customer)
 admin.site.register(Director)
 admin.site.register(Queue)
 admin.site.register(Category)
+admin.site.register(Queueoperator)
