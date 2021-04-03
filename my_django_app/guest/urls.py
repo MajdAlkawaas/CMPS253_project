bb2c30468669413c1f5d0822eb3c6c2f0d65484a
Majd Alkawaas <mma250@mail.aub.edu>
2021-04-03 15:20:16 +0300

    Removed an old outdated URL

--- my_django_app/guest/urls.py
+++ my_django_app/guest/urls.py
@@ -5,12 +5,7 @@ urlpatterns = [
     # Request a queue guest page using queue_uuid
     path('customer/uuid/<uuid:director_uuid>', views.guest_view_uuid, name="queue-page"),
 
-    # Request a queue guest page using queue_id
-    path('queue/id/<int:queue_id>', views.guest_view_id, name="queue-page"),
-
-
     path('guest/', views.guest, name="guest-page"),
-
     path('guestWaitingPage', views.guest_waiting_page, name="guest-waiting-page"),
 
 ]
