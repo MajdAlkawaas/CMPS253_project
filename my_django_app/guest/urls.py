1edf96f118c98ad59b5fc85722c00d721a0f3323
hre06 <65748557+hre06@users.noreply.github.com>
2021-04-16 02:14:17 +0300

    Added the guest id to the url of the guestWaitingPage

--- my_django_app/guest/urls.py
+++ my_django_app/guest/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [   
     # Request a queue guest page using queue_uuid
     path('customer/uuid/<uuid:director_uuid>', views.guest_view_uuid, name="guest-page"),
-    path('guestWaitingPage', views.guest_waiting_page, name="guest-waiting-page"),
+    path('guestWaitingPage/<int:guest_id>', views.guest_waiting_page, name="guest-waiting-page"),
 
 ]
