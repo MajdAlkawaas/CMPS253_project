6d8aefa5a33dbc28d49bcd7bb1361fed58652b55
Ahmed Sadaqa <afs18@mail.aub.edu>
2021-04-25 08:33:56 +0300

    Update settings.py

--- my_django_app/my_django_app/settings.py
+++ my_django_app/my_django_app/settings.py
@@ -201,8 +201,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'customer.User'
 # STATIC_DIR = ''
