80ce9d046e76787f4f4416f51b1cab7f729669c7
Majd Alkawaas <mma250@mail.aub.edu>
2021-04-26 16:44:07 +0300

    Removed the guest app static files.

--- my_django_app/guest/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Guest
-admin.site.register(Guest)
--- my_django_app/guest/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [   
-    # Request a queue guest page using queue_uuid
-    path('customer/uuid/<uuid:director_uuid>', views.guest_view_uuid, name="guest-page"),
-    path('guestWaitingPage/<int:guest_id>', views.guest_waiting_page, name="guest-waiting-page"),
-]
