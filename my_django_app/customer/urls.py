2aee4b1ae9e24e13eff19057f2e0fb9123e0d8f7
Ahmed Sadaqa <afs18@mail.aub.edu>
2021-04-27 17:13:43 +0300

    Update urls.py

--- my_django_app/customer/urls.py
+++ my_django_app/customer/urls.py
@@ -11,14 +11,14 @@ urlpatterns = [
 
     path('password_reset/', 
         auth_views.PasswordResetView.as_view(
-            template_name='customer/registration/forgot.html',
+            template_name='Customer/registration/forgot.html',
             form_class=UserPasswordResetForm),
         name='password_reset'
         ),
     
     path('reset/<uidb64>/<token>/', 
         auth_views.PasswordResetConfirmView.as_view(
-            template_name="customer/registration/password_reset_confirm.html",
+            template_name="Customer/registration/password_reset_confirm.html",
             form_class=UserSetPasswordForm), 
         name='password_reset_confirm'
         ),
@@ -27,14 +27,14 @@ urlpatterns = [
 
     path('password_reset/done/', 
         auth_views.PasswordResetDoneView.as_view(
-            template_name='customer/registration/password_reset_done.html'), 
+            template_name='Customer/registration/password_reset_done.html'), 
         name='password_reset_done'
         ),
     
     
     path('reset/done/', 
         auth_views.PasswordResetCompleteView.as_view(
-            template_name='customer/registration/password_reset_complete.html'), 
+            template_name='Customer/registration/password_reset_complete.html'), 
         name='password_reset_complete'
         ),  
 
